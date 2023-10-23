12a9d7ef59a567c93666875bbf806d0fd419a103
GonçaloCruz <a104346@alunos.uminho.pt>
2023-10-23 17:42:25 +0100

    Pequenas alterações

--- include/Parser.h
+++ include/Parser.h
@@ -63,56 +63,6 @@ void validate_user(User_temp *nova, char parametros[][FIELD_SIZE]);
     //    |->função que verifica parâmetro a parâmetro de cada elemento do array de utilizadores, e caso algum elemento seja inválido
     //       a função altera o parâmetro que indica se esse elemento é válido ou não para 0
 
-int compare_date_time (char *i, char *f);
-    //    |->função que compara duas datas e retorna 1 caso a primeira data seja a mais antiga
-
-int verify_email (char *e);
-    //    |->função que verifica se um email que lhe é passado como argumento é válido ou não de acordo com as regras especificadas
-
-int verify_account_status(char *status);
-    //    |->função que verifica se o status de um utilizador é ativo (1) ou inativo (0) ou for inválido (2)
-
-int verify_airport (char *airport);
-    //    |->função que verifica se o nome dum aeroporto tem 3 letras 
-
-int verify_breakfast(char *breakfast);
-    //    |->função que verifica se o breakfast está incluido (1) ou não (0) ou é inválido (2) 
-
-int verify_country_code (char *c);
-    //    |->função que verifica se o country_code é constituído por exatamente duas letras
-
-int count_passengers (Contador_id *contador_array, int num_linhas_contador, char parametros[][FIELD_SIZE]);
-    //    |->função que indica o número de passageiros de um voo
-
-int compare_seats_passengers (char *seats, int num_passengers);
-    //    |->função que compara o número de assentos num avião com o número de passageiros nesse mesmo avião
-
-int verify_hotel_stars (char *s);
-    //    |->função que verifica se o número de estrelas de um hotel é válido
-
-int verify_city_tax (char *t);
-    //    |->função que verifica se o imposto que se paga numa cidade é válido 
-
-char* strnCpy(char *d, char *s, int n, int p);
-    //    |->função semelhante à "strcpy" mas esta modificada consegue copiar uma string para uma posição específica de outra string
-
-int check_date(char *d);
-    //    |->função que verifica se uma data é válida
-
-int check_time(char *t);
-    //    |->função que verifica se uma hora é válida
-
-int check_dateWtime(char *dateT);
-    //    |->função que verifica se uma data com hora é válida
-
-int check_price_per_night(char *p);
-    //    |->função que verifica se o preço por noite de estadia num hotel é válido
-
-int check_reserva_rating(char *r);
-    //    |->função que verifica se as classificações de reserva de um hotel são válidas
-
-int check_length(char *s);
-    //    |->função que verifica se uma string é vazia ou não
 
 void open_files();
 
